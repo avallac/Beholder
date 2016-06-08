48bf7404d06c5655a8675b07dcea07eddb81a677
Petr Petrenko <ppetrenko@xiag.ch>
2016-06-08 16:27:55 +0600

    add phpunit

---
+++ test/unit/CaseTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PhpAmqDaemonManager;
+
+class CaseTest extends \PHPUnit_Framework_TestCase
+{
+    public function testOneIsOne()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
