4fd95a75c6a7316e49a6ca6f9fffa5f22c8b9e35
Petr Petrenko <ppetrenko@xiag.ch>
2016-06-14 21:24:14 +0600

    rename val

--- examples/beholder.php
+++ examples/beholder.php
@@ -6,8 +6,8 @@ use \Beholder\Beholder;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest', '/');
-$agent = new Beholder($connection, 'testAdmin');
-$agent->addHandler(function ($host, $role, $status) {
+$beholder = new Beholder($connection, 'testAdmin');
+$beholder->addHandler(function ($host, $role, $status) {
     print "change in bd host:".$host." role:".$role." status".$status."\n";
 });
-$agent->run();
+$beholder->run();
--- examples/mionion.php
+++ examples/mionion.php
@@ -8,11 +8,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/Message/SimpleMessage.php';
 
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest', '/');
-$agent = new Minion($connection, 'testhost', 'testAdmin');
-$agent->bindRole('parser', 'parserQ');
-$agent->getRole('parser')['messageManager']->bind(new SimpleMessage(
+$minion = new Minion($connection, 'testhost', 'testAdmin');
+$minion->bindRole('parser', 'parserQ');
+$minion->getRole('parser')['messageManager']->bind(new SimpleMessage(
     function ($m) {
         var_dump($m);
     }
 ));
-$agent->run();
+$minion->run();
