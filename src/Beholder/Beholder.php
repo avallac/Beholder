5dc1a1c7eaba33560f0ba29fea9a8fe66755d761
Petr Petrenko <ppetrenko@xiag.ch>
2016-09-21 02:52:56 +0700

    sec from last request

--- src/Beholder/Beholder.php
+++ src/Beholder/Beholder.php
@@ -63,7 +63,7 @@ class Beholder
                 $this->sendUpdateStatusToMinion($m->get('hostname'), $m->get('role'));
             }
             $this->minions[$m->get('hostname')][$m->get('role')]['curStatus'] = $m->get('status');
-            $this->minions[$m->get('hostname')][$m->get('role')]['lastUpdate'] = microtime(true);
+            $this->minions[$m->get('hostname')][$m->get('role')]['absoluteLastUpdate'] = microtime(true);
         }));
 
         $this->messageManager->bind(new BeholderAdminStatus(function (MQMessage $m) {
@@ -72,7 +72,13 @@ class Beholder
         }));
 
         $this->messageManager->bind(new BeholderStatusGet(function (MQMessage $message) {
+            $currentTime = microtime(true);
             $q = $message->get('queue');
+            foreach ($this->minions as $host) {
+                foreach ($host as &$item) {
+                    $item['lastUpdate'] = $currentTime - $item['absoluteLastUpdate'];
+                }
+            }
             $this->createMessage(new AdminMinionStatus(), ['minions' => $this->minions], $q);
         }));
 
